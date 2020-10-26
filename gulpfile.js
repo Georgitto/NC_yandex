b88455138bf60a1fd0bf2fd4f7808e09a41cc911
Georgiy Shaporenko <gshaporenko200@mail.ru>
2020-10-26 19:39:30 +0300

    Test

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path = {
+    build: {
+        html: project_folder + "/html/",
+        css: project_folder + "/css/",
+        img: project_folder + "/img/",
+        js: project_folder + "/js/",
+        fonts: project_folder + "/fonts/"
+    },
+    src: {
+        html: source_folder + "/html/",
+        css: source_folder + "/css/",
+        img: source_folder + "/img/",
+        js: source_folder + "/js/",
+        fonts: source_folder + "/fonts/"
+    }
+}
